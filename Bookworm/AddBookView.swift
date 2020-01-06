829bc7aebb28a6715e888068614ac1f5ac743b91
Jules Lee <jackconsolidated@gmail.com>
2020-01-06 19:05:46 +0800

    Testing core data by creating a list of students

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,66 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Jules Lee on 1/6/20.
+//  Copyright © 2020 Jules Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horrorr", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {_ in
+                            Text("\(0)")
+                        }
+                    }
+                    
+                    TextField("Write a review", text: $review)
+                }
+                
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: self.moc)
+                        newBook.title = self.title
+                        newBook.author = self.author
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        newBook.review = self.review
+                        try? self.moc.save()
+                    }
+                }
+            }
+        }
+        .navigationBarTitle("Add Book")
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            List {
+                ForEach(students, id: \.id) { student in
+                    Text(student.name ?? "Unknown")
+                }
+            }
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermoine", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+                
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: self.moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? self.moc 
+            }
+        }
     }
 }
 
